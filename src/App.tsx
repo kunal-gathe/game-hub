1895d274095433fb26cbdd540371f2193a9cc7e6
Kunal Gathe <kunalgathe121@example.com>
2023-04-09 16:29:20 +0530

    responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 import React from 'react'
-import Navbar from './components/Navbar'
-import GameGrid from './components/GameGrid'
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <GameGrid />
-    </div>
-  )
+  return <Grid templateAreas={{
+    base: `"nav" "main"`,
+    lg: `"nav nav" "aside main"`
+  }}>
+    <GridItem area='nav' bg = 'coral'>Nav</GridItem>
+    <Show above='lg'>
+    <GridItem area='aside' bg = 'gold'>aside</GridItem>
+    </Show>
+    <GridItem area='main' bg = 'dodgerblue'>Nav</GridItem>
+  </Grid>
 }
 
 export default App
