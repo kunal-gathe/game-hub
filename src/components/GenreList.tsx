90bd332787c78527fae01ab62e663aa7fe3bb8ac
Kunal Gathe <kunalgathe121@example.com>
2023-04-10 16:34:24 +0530

    data fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+
 import useGenres from '../hooks/useGenre'
 
+
 function GenreList() {
-  const {genres} = useGenres();
+  const {data} = useGenres();
   return (
     <ul>
-      {genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
+      {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
     </ul>
   )
 }
