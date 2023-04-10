21c74d1522464d7b2efef9e94cdd84679a3a6192
Kunal Gathe <kunalgathe121@example.com>
2023-04-10 16:10:51 +0530

    display icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react'
 import React from 'react'
 import GenreList from './components/GenreList'
+import GameCard from './components/GameCard'
+import Navbar from './components/Navbar'
 
 function App() {
   return <Grid templateAreas={{
     base: `"nav" "main"`,
     lg: `"nav nav" "aside main"`
   }}>
-    <GridItem area='nav' bg = 'coral'>Nav</GridItem>
+    <GridItem area='nav' bg = 'coral'>
+      <Navbar />
+    </GridItem>
     <Show above='lg'>
     <GridItem area='aside' bg = 'gold'>
-      <GenreList />
+      {/* <GameCard /> */}
     </GridItem>
     </Show>
     <GridItem area='main' bg = 'dodgerblue'>Nav</GridItem>
