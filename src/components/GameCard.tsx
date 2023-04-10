7befb02ac2b93fdb056781c15061979e2b0bfb5f
Kunal Gathe <kunalgathe121@example.com>
2023-04-10 17:10:42 +0530

    optimize image

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import { Game } from '../hooks/useGames'
 import PlatormIconList from './PlatormIconList'
+import getCroppedImageUrl from '../servicess/imageUrl'
 
 interface Props {
     game: Game
@@ -10,7 +11,7 @@ interface Props {
 const GameCard = ({ game }: Props) =>{
     return (
         <Card borderRadius={10} overflow='hidden'>
-            <Image src={game.background_image} />
+            <Image src={getCroppedImageUrl (game.background_image)} />
             <CardBody >
                 <Heading fontSize='2xl'>{game.name}</Heading>
              <PlatormIconList platforms={game.parent_platforms.map(p => p.platform)} />
